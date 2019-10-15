f4804a4dc368bcfec31a75dbd3132ccaecbcee1c
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-10-15 11:36:59 +0200

    First commit

---
+++ components/GameContainer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>HALLOO THIS IS MY GAME BOX</h1>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <img class="banner" src="../assets/header_banner.png" alt="Banner of flags">
+    <GameContainer />
+    <GameContainer />
+    <GameContainer />
+  </div>
+</template>
+
+<script>
+import GameContainer from '../components/GameContainer'
+export default {
+  components: {
+    GameContainer
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+img {
+
+}
+</style>
